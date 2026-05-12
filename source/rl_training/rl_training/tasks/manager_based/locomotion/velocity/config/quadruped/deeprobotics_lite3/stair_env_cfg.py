b7c95c487c8bae9f5fd996194f513e691666e1c8
magna-git <diakhadijetou96@gmail.com>
2026-05-12 17:29:28 +0200

    stair v3 - flat_orientation -3.5, feet_air_time_var -2.0, vitesse réduite 0.55, base_height 0.38

--- source/rl_training/rl_training/tasks/manager_based/locomotion/velocity/config/quadruped/deeprobotics_lite3/stair_env_cfg.py
+++ source/rl_training/rl_training/tasks/manager_based/locomotion/velocity/config/quadruped/deeprobotics_lite3/stair_env_cfg.py
@@ -13,31 +13,32 @@ class DeeproboticsLite3StairEnvCfg(DeeproboticsLite3RoughEnvCfg):
         self.scene.terrain.terrain_generator.sub_terrains["hf_pyramid_slope"].proportion = 0.05
         self.scene.terrain.terrain_generator.sub_terrains["hf_pyramid_slope_inv"].proportion = 0.00
         self.scene.terrain.terrain_generator.sub_terrains["boxes"].proportion = 0.0
-        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs"].proportion = 0.55 # c''etait a 50 avant 
-        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs_inv"].proportion = 0.10
+        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs"].proportion = 0.65 # c''etait a 50 avant 
+        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs_inv"].proportion = 0.0
 
         # ------------------------------ Rewards : escalier focused ------------------------------
         self.rewards.lin_vel_z_l2.weight = -1.0            # corps peut monter
-        self.rewards.ang_vel_xy_l2.weight = -0.03           # corps peut s'incliner
-        self.rewards.flat_orientation_l2.weight = -2.5       # dos peut s'incliner
+        self.rewards.ang_vel_xy_l2.weight = -0.045           # corps peut s'incliner
+        self.rewards.flat_orientation_l2.weight = -3.5       # dos peut s'incliner
         self.rewards.feet_height.params["target_height"] = 0.16  # marches de 15.5cm
         self.rewards.feet_air_time.params["threshold"] = 0.55
         self.rewards.feet_gait.weight = 1.0                  # forcer trot diagonal
         self.rewards.joint_mirror.weight = -0.05             # symétrie souple
-        self.rewards.feet_air_time_variance.weight = 0.0    # rythme libre
+        self.rewards.feet_air_time_variance.weight = -0.02
+           # rythme libre
         self.rewards.base_height_l2.weight = -15.0 # j'ai changer egalement ses deux -10 
-        self.rewards.base_height_l2.params["target_height"] = 0.37
+        self.rewards.base_height_l2.params["target_height"] = 0.38
 
 
 
         # Anti-stumble : pénalise les pieds qui tapent les marches
-        self.rewards.feet_stumble.weight = -0.2
+        self.rewards.feet_stumble.weight = -0.3
         self.rewards.feet_stumble.params["sensor_cfg"].body_names = [self.foot_link_name]
 
         # ------------------------------ Commands : vitesse prudente --------------------------------
-        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.7)
-        self.commands.base_velocity.ranges.lin_vel_y = (-0.3, 0.3)
-        self.commands.base_velocity.ranges.ang_vel_z = (-0.3, 0.3)
+        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.55) ## il etais a 7 
+        self.commands.base_velocity.ranges.lin_vel_y = (-0.2, 0.2)
+        self.commands.base_velocity.ranges.ang_vel_z = (-0.2, 0.2)
 
         # ------------------------------ Disable zero rewards --------------------------------------
         if self.__class__.__name__ == "DeeproboticsLite3StairEnvCfg":
