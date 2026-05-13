2bfeb9cc22eacbbd6f545a749aeec98521cb4e3e
magna-git <diakhadijetou96@gmail.com>
2026-05-13 16:44:21 +0200

    new stair v2 - 13 mai - anti-rebond lin_vel_z -2.5, air_time 0.62, flat -3.5, slide -0.1, gait 1.5, base_height -15.0/0.36, test sim-to-real validé

--- source/rl_training/rl_training/tasks/manager_based/locomotion/velocity/config/quadruped/deeprobotics_lite3/stair_env_cfg.py
+++ source/rl_training/rl_training/tasks/manager_based/locomotion/velocity/config/quadruped/deeprobotics_lite3/stair_env_cfg.py
@@ -9,26 +9,23 @@ class DeeproboticsLite3StairEnvCfg(DeeproboticsLite3RoughEnvCfg):
         super().__post_init__()
 
         # ------------------------------ Terrains : majorité escaliers ------------------------------
-        self.scene.terrain.terrain_generator.sub_terrains["random_rough"].proportion = 0.30
+        self.scene.terrain.terrain_generator.sub_terrains["random_rough"].proportion = 0.35
         self.scene.terrain.terrain_generator.sub_terrains["hf_pyramid_slope"].proportion = 0.05
         self.scene.terrain.terrain_generator.sub_terrains["hf_pyramid_slope_inv"].proportion = 0.00
         self.scene.terrain.terrain_generator.sub_terrains["boxes"].proportion = 0.0
-        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs"].proportion = 0.65 # c''etait a 50 avant 
+        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs"].proportion = 0.60
         self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs_inv"].proportion = 0.0
 
         # ------------------------------ Rewards : escalier focused ------------------------------
-        self.rewards.lin_vel_z_l2.weight = -1.0            # corps peut monter
-        self.rewards.ang_vel_xy_l2.weight = -0.045           # corps peut s'incliner
-        self.rewards.flat_orientation_l2.weight = -3.5       # dos peut s'incliner
+        self.rewards.lin_vel_z_l2.weight = -2.5           # corps peut monter
+        self.rewards.ang_vel_xy_l2.weight = -0.1           # corps peut s'incliner
+        self.rewards.flat_orientation_l2.weight = -2.0     # dos peut s'incliner
         self.rewards.feet_height.params["target_height"] = 0.16  # marches de 15.5cm
-        self.rewards.feet_air_time.params["threshold"] = 0.55
-        self.rewards.feet_gait.weight = 1.0                  # forcer trot diagonal
-        self.rewards.joint_mirror.weight = -0.05             # symétrie souple
-        self.rewards.feet_air_time_variance.weight = -0.02
-           # rythme libre
-        self.rewards.base_height_l2.weight = -15.0 # j'ai changer egalement ses deux -10 
-        self.rewards.base_height_l2.params["target_height"] = 0.38
-
+        self.rewards.feet_air_time.params["threshold"] = 0.62 ## c'est le temps  qu'il faut pour qu'une pattes raméne ses pattes
+        self.rewards.feet_gait.weight = 1.5                  # forcer trot diagonal
+        self.rewards.joint_mirror.weight = -0.07            # symétrie souple
+        self.rewards.feet_air_time_variance.weight = -4.0   # rythme libre
+        self.rewards.base_height_l2.params["target_height"] = 0.36
 
 
         # Anti-stumble : pénalise les pieds qui tapent les marches
@@ -36,9 +33,10 @@ class DeeproboticsLite3StairEnvCfg(DeeproboticsLite3RoughEnvCfg):
         self.rewards.feet_stumble.params["sensor_cfg"].body_names = [self.foot_link_name]
 
         # ------------------------------ Commands : vitesse prudente --------------------------------
-        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.55) ## il etais a 7 
-        self.commands.base_velocity.ranges.lin_vel_y = (-0.2, 0.2)
-        self.commands.base_velocity.ranges.ang_vel_z = (-0.2, 0.2)
+        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.6)
+        self.commands.base_velocity.ranges.lin_vel_y = (-0.10, 0.10)
+        self.commands.base_velocity.ranges.ang_vel_z = (-0.10, 0.10)
+        ##premiere note je dis autgmenter la vitesse 
 
         # ------------------------------ Disable zero rewards --------------------------------------
         if self.__class__.__name__ == "DeeproboticsLite3StairEnvCfg":
