8f90c3599c67868bb62c2914e1bac05f260f54f1
magna-git <diakhadijetou96@gmail.com>
2026-05-18 10:37:00 +0200

    resume from stair_good_v2 - threshold 0.57, vel_y/z ±0.25, lin_vel_x 0.75, anti-rebond lin_vel_z -2.5

--- source/rl_training/rl_training/tasks/manager_based/locomotion/velocity/config/quadruped/deeprobotics_lite3/stair_env_cfg.py
+++ source/rl_training/rl_training/tasks/manager_based/locomotion/velocity/config/quadruped/deeprobotics_lite3/stair_env_cfg.py
@@ -18,24 +18,25 @@ class DeeproboticsLite3StairEnvCfg(DeeproboticsLite3RoughEnvCfg):
 
         # ------------------------------ Rewards : escalier focused ------------------------------
         self.rewards.lin_vel_z_l2.weight = -2.5           # corps peut monter
-        self.rewards.ang_vel_xy_l2.weight = -0.1           # corps peut s'incliner
-        self.rewards.flat_orientation_l2.weight = -2.0     # dos peut s'incliner
+        self.rewards.ang_vel_xy_l2.weight = -0.05         # corps peut s'incliner
+        self.rewards.flat_orientation_l2.weight = -3.5     # dos peut s'incliner
         self.rewards.feet_height.params["target_height"] = 0.16  # marches de 15.5cm
-        self.rewards.feet_air_time.params["threshold"] = 0.62 ## c'est le temps  qu'il faut pour qu'une pattes raméne ses pattes
+        self.rewards.feet_air_time.params["threshold"] = 0.57 ## c'est le temps  qu'il faut pour qu'une pattes raméne ses pattes
         self.rewards.feet_gait.weight = 1.5                  # forcer trot diagonal
         self.rewards.joint_mirror.weight = -0.07            # symétrie souple
-        self.rewards.feet_air_time_variance.weight = -4.0   # rythme libre
+        self.rewards.feet_air_time_variance.weight = -2.0   # rythme libre
         self.rewards.base_height_l2.params["target_height"] = 0.36
-
+        self.rewards.feet_slide.weight = -0.1 # pour qu'il ne glisse pas 
+        self.rewards.base_height_l2.weight = -15.0
 
         # Anti-stumble : pénalise les pieds qui tapent les marches
         self.rewards.feet_stumble.weight = -0.3
         self.rewards.feet_stumble.params["sensor_cfg"].body_names = [self.foot_link_name]
 
         # ------------------------------ Commands : vitesse prudente --------------------------------
-        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.6)
-        self.commands.base_velocity.ranges.lin_vel_y = (-0.10, 0.10)
-        self.commands.base_velocity.ranges.ang_vel_z = (-0.10, 0.10)
+        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.74)
+        self.commands.base_velocity.ranges.lin_vel_y = (-0.25, 0.25)
+        self.commands.base_velocity.ranges.ang_vel_z = (-0.25, 0.25)
         ##premiere note je dis autgmenter la vitesse 
 
         # ------------------------------ Disable zero rewards --------------------------------------
