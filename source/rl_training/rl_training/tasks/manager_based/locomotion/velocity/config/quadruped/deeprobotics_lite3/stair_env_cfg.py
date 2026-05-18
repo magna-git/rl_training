6456eeeaa3e4c7fe42885043528eba63f3c1dfae
magna-git <diakhadijetou96@gmail.com>
2026-05-18 16:57:20 +0200

    prep stair v4 from v3 - joint_mirror -0.09, stairs_inv 0.10, threshold 0.55, vitesse 0.70, anti-penchement gauche

--- source/rl_training/rl_training/tasks/manager_based/locomotion/velocity/config/quadruped/deeprobotics_lite3/stair_env_cfg.py
+++ source/rl_training/rl_training/tasks/manager_based/locomotion/velocity/config/quadruped/deeprobotics_lite3/stair_env_cfg.py
@@ -9,21 +9,21 @@ class DeeproboticsLite3StairEnvCfg(DeeproboticsLite3RoughEnvCfg):
         super().__post_init__()
 
         # ------------------------------ Terrains : majorité escaliers ------------------------------
-        self.scene.terrain.terrain_generator.sub_terrains["random_rough"].proportion = 0.35
+        self.scene.terrain.terrain_generator.sub_terrains["random_rough"].proportion = 0.30
         self.scene.terrain.terrain_generator.sub_terrains["hf_pyramid_slope"].proportion = 0.05
         self.scene.terrain.terrain_generator.sub_terrains["hf_pyramid_slope_inv"].proportion = 0.00
         self.scene.terrain.terrain_generator.sub_terrains["boxes"].proportion = 0.0
-        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs"].proportion = 0.60
-        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs_inv"].proportion = 0.0
+        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs"].proportion = 0.45
+        self.scene.terrain.terrain_generator.sub_terrains["pyramid_stairs_inv"].proportion = 0.20
 
         # ------------------------------ Rewards : escalier focused ------------------------------
         self.rewards.lin_vel_z_l2.weight = -2.5           # corps peut monter
         self.rewards.ang_vel_xy_l2.weight = -0.05         # corps peut s'incliner
         self.rewards.flat_orientation_l2.weight = -3.5     # dos peut s'incliner
         self.rewards.feet_height.params["target_height"] = 0.16  # marches de 15.5cm
-        self.rewards.feet_air_time.params["threshold"] = 0.57 ## c'est le temps  qu'il faut pour qu'une pattes raméne ses pattes
+        self.rewards.feet_air_time.params["threshold"] = 0.55 ## c'est le temps  qu'il faut pour qu'une pattes raméne ses pattes
         self.rewards.feet_gait.weight = 1.5                  # forcer trot diagonal
-        self.rewards.joint_mirror.weight = -0.07            # symétrie souple
+        self.rewards.joint_mirror.weight = -0.09            # symétrie souple
         self.rewards.feet_air_time_variance.weight = -2.0   # rythme libre
         self.rewards.base_height_l2.params["target_height"] = 0.36
         self.rewards.feet_slide.weight = -0.1 # pour qu'il ne glisse pas 
@@ -34,7 +34,7 @@ class DeeproboticsLite3StairEnvCfg(DeeproboticsLite3RoughEnvCfg):
         self.rewards.feet_stumble.params["sensor_cfg"].body_names = [self.foot_link_name]
 
         # ------------------------------ Commands : vitesse prudente --------------------------------
-        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.74)
+        self.commands.base_velocity.ranges.lin_vel_x = (0.0, 0.70)
         self.commands.base_velocity.ranges.lin_vel_y = (-0.25, 0.25)
         self.commands.base_velocity.ranges.ang_vel_z = (-0.25, 0.25)
         ##premiere note je dis autgmenter la vitesse 
